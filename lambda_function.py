dfa46aebce34f8c3198dc12369b0c09d45711646
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-12-29 21:43:20 +0900

    first commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,52 +1,27 @@
 import boto3
 from boto3.dynamodb.conditions import Key,Attr
 
-def put_item(table):
-  table.put_item(
-    Item = {
-      "PartitionKey": "shop_1",
-      "SortKey": "shop_1"
-    }
-  )
 
 def main():
   dynamoDB = boto3.resource("dynamodb")
-  print("type(dynamoDB)")
-  print(type(dynamoDB))
-  print("dynamoDB")
-  print(dynamoDB)
-  print("-----------------")
   table = dynamoDB.Table("ESM")
-  print("type(table)")
-  print(type(table))
-  print("table")
-  print(table)
-  print("-----------------")
-
   query_data = table.query(
-    KeyConditionExpression = Key("PK").eq("shop_1")
+    IndexName='GSI1',
+    KeyConditionExpression = Key("SK").eq("shop") & Key("PK").begins_with("shop_")
   )
-  print("type(query_data)")
-  print(type(query_data))
-  print("query_data")
   print(query_data)
-  print("-----------------")
-
   shops = query_data["Items"]
-  print("type(shops)")
-  print(type(shops))
-  print("shops")
-  print(shops)
-  print("-----------------")
-  for shop in shops:
-    print("type(shop)")
-    print(type(shop))
-    print(shop)
-    print(shop["PK"])
-    print("-----------------")
+  # print(shops)
+  return shops
+
 
 def lambda_handler(event, context):
   try:
-    main()
+    shops = main()
+    response = {
+      "statuscode": 200,
+      "data": shops
+    }
+    return response
   except Exception as e:
     print("Error")
